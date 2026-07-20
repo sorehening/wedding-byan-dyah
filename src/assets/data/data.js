58cf2445678c1c7f6f1f423227fb3256422e02ae
sorehening <andreybihan@gmail.com>
2026-07-20 21:05:38 +0700

    Wedding Byan & Dyah

---
+++ src/assets/data/data.js
@@ -0,0 +1,138 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'Tasbihan Pratama',
+            child: '-',
+            father: '-',
+            mother: '-',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Dyah Awatari R.',
+            child: '-',
+            father: '-',
+            mother: '-',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/couple.png'
+    },
+
+    time: {
+        marriage: {
+            year: '2026',
+            month: 'Agustus',
+            date: '7',
+            day: 'Jumat',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+
+            
+            }
+            
+        
+            
+
+    },
+
+        
+        reception: {
+            year: '2026',
+            month: 'Agustus',
+            date: '9',
+            day: 'Minggu',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Gedung Barokah, Jl. Sempurna No.147 Dusun I, Sambirejo Timur, Kec. Percut Sei Tuan, Kabupaten Deli Serdang, Sumatera Utara 20371'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/PyggnCFsJcAtXHcN7',
+        map: 'https://maps.app.goo.gl/VNXPf5LA4bnyJfkH9',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        },
+        {
+            id: 6,
+            image: './src/assets/images/6.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'Dyah Awatari R.',
+            icon: './src/assets/images/bca.png',
+            rekening: '901710007173'
+        },
+        {
+            id: 2,
+            name: 'Dyah Awatari R.',
+            icon: './src/assets/images/bri.png',
+            rekening: '109493314684'
+        },
+    ],
+
+    audio: './src/assets/audio/wedding.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbwubbih2BIMTEArQFy2Uqu-jWYshKN2Ab2Qsn0uhR7_45KxPkg8GWHt2AF7S9nK1qAs/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
