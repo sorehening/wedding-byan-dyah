5874ec03c319729306fe19a9470936425035d2dc
sorehening <andreybihan@gmail.com>
2026-07-31 19:36:06 +0700

    Update time.js

--- src/js/time.js
+++ src/js/time.js
@@ -2,9 +2,9 @@ import { data } from "../assets/data/data.js";
 
 export const time = () => {
     const timeContainer = document.querySelector('.time');
-    const [receptionDiv] = timeContainer.querySelectorAll('div div');
-    const mapLink = timeContainer.querySelector('p');
-    const addressParagraph = timeContainer.querySelector('p');
+    const [marriageDiv, receptionDiv] = timeContainer.querySelectorAll('div div');
+    const mapLink = timeContainer.querySelector('a');
+    const addressParagraph = timeContainer.querySelector('a + p');
 
     const createTimeListItem = (title, details) => (
         `<h3>${title}</h3>
@@ -12,7 +12,7 @@ export const time = () => {
          Pukul ${details.hours.start} WIB sd ${details.hours.finish}</p>`
     );
 
-    
+    marriageDiv.innerHTML = createTimeListItem('Akad', data.time.marriage);
     receptionDiv.innerHTML = createTimeListItem('Resepsi', data.time.reception);
 
     mapLink.href = data.link.map;
